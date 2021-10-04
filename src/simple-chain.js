5121c37ae035e8be31f62cd8cb2c416f5417bf3f
spiderVS <fuzzy003@gmail.com>
2021-10-04 09:41:07 +0300

    feat: add solve for chain maker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,27 +2,46 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 export default {
+
+  chainArr: [],
+
+  resultStr: '',
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chainArr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    this.resultStr = '';
+    if (value === undefined) this.chainArr.push('');
+    else this.chainArr.push(`${value}`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if ((!position) || (typeof(position) !== 'number') || (this.chainArr[position] === undefined)) {
+      this.chainArr = [];
+      throw new Error("You can\'t remove incorrect link!");
+    } else this.chainArr.splice(position-1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chainArr.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      for (let i = 0; i <= this.chainArr.length-2; i++ ){
+          if (this.chainArr[i] == '')  this.resultStr += `( )~~`;
+            else this.resultStr += `( ${this.chainArr[i]} )~~`;
+      }
+      this.resultStr += `( ${this.chainArr[this.chainArr.length-1]} )`;
+      this.chainArr = [];
+    return this.resultStr;
   }
+
 };
